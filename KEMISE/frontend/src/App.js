6b437d8bd11979fb701008fb7f87579a8a7c243d
yossef-G <yossef.g.09@gmail.com>
2022-10-14 18:37:34 +0300

    Create-React-App

---
+++ KEMISE/frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">kemise</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
