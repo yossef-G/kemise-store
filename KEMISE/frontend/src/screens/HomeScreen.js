a787c6b9f4a7f64df24bc5a3bd956ea16387a573
yossef-G <yossef.g.09@gmail.com>
2022-10-19 15:10:02 +0300

    fetch products

--- KEMISE/frontend/src/screens/HomeScreen.js
+++ KEMISE/frontend/src/screens/HomeScreen.js
@@ -1,20 +1,31 @@
-import data from '../data';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+//import data from '../data';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       {' '}
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
-            <a href={`/product/${product.slug}`}>
+            <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
-            </a>
+            </Link>
             <div className="product-info">
-              <a href={`/product/${product.slug}`}>
+              <Link to={`/product/${product.slug}`}>
                 <p>{product.name}</p>
-              </a>
+              </Link>
               <p>
                 <strong>${product.price}</strong>
               </p>
