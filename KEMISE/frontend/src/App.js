4a83a34d9b371f7f07c2de9f9c6ed0839f1d3ca7
yossef-G <yossef.g.09@gmail.com>
2022-10-24 07:21:17 +0300

    updating data + adding bootstrap

--- KEMISE/frontend/src/App.js
+++ KEMISE/frontend/src/App.js
@@ -1,20 +1,51 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <a href="/">kemise</a>
+          <Navbar bg="primary" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>
+                  <h2>Kemis</h2>
+                </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
-        <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen/>} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+        <main className="">
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer className="p-2 mb-2 bg-secondary text-blue">
+          <div className="container p-4 pb-0">
+            <section>
+              <p className="d-flex justify-content-center align-items-center ms-auto p-2 bd-highlight">
+                <span className="me-1 h4">Register for free</span>
+                <button
+                  type="button"
+                  className="btn btn-outline-info btn-light btn-rounded"
+                >
+                  Sign up!
+                </button>
+                <div className="ms-auto h4 p-2 bd-highlight">
+                  all-right-reserved@
+                </div>
+              </p>
+            </section>
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
